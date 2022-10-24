8d33f1acaf8fdeb6934d0f4d5eda48adf9985041
david-moravec <64803040+david-moravec@users.noreply.github.com>
2022-10-24 11:24:49 +0000

    Add robot 'object'

---
+++ robot.hs
@@ -0,0 +1,18 @@
+robot (n,a,h) = \message -> message (n,a,h)
+
+getName robot = robot (\(n,a,h) -> n)
+getAttack robot = robot (\(n,a,h) -> a)
+getHealth robot = robot (\(n,a,h) -> h)
+
+setName robot newName = robot (\(n,a,h) -> (newName,a,h))
+setAttack robot newAttack = robot (\(n,a,h) -> (n,newAttack,h))
+setHealth robot newHealth = robot (\(n,a,h) -> (n,a,newHealth))
+
+printRobot robot = ()
+
+damage aRobot attackDamage = aRobot (\(n,a,h) -> (n,a,h-attackDamage))
+
+fight aRobot defender = damage defender attack
+  where attack = if getHP aRobot > 10
+                 than getAttack aRobot
+                 else 0
